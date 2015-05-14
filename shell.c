9e4254c67ff598e50fe21837601987ad75101f6f
joyaa <joy.alam91@gmail.com>
2015-05-14 12:35:41 +0200

    Nothing is working

--- shell.c
+++ shell.c
@@ -6,23 +6,42 @@
 #include <readline/history.h>
 #include <readline/readline.h>
 #include <time.h>
+#include <sys/types.h>
+
+static int numArgs = 0;	//number of arguments (including command)
 
 char **tokenize_line(char*);
 int exec_line(char**);
-static int numArgs = 0;
 
 int main(int argc, char **argv) {
 	char *line;
 	char **args;
 	int status;
+	pid_t pid;
 
 	while(1) {
+		printf("PID%i", pid);
+		while((pid = waitpid(-1, &status, WNOHANG)) > 0) {
+			printf("Sug balle1");
+			if(WIFEXITED(status) || WIFSIGNALED(status)) {
+				printf("Process %d has been terminated\n", pid);
+			}
+		}
 		line = readline("> "); // readline return is default malloc
 		args = tokenize_line(line);
 		status = exec_line(args);
+		while((pid = waitpid(-1, &status, WNOHANG)) > 0) {
+			printf("Sug balle2");
+			if(WIFEXITED(status) || WIFSIGNALED(status)) {
+				printf("Process %d has been terminated\n", pid);
+			}
+		}
+
+
+		//free allocated memory
 		free(line);
 		free(args);					
-	}    
+	}
 }
 
 char **tokenize_line(char *line) {
@@ -60,33 +79,41 @@ int exec_line(char **args) {
 	time_t start, stop;	
 	double totalTime;
 
-	printf("%d", numArgs);
+	//check if foreground or background process
 	if(*args[numArgs-1] == '&') {
+		//background process
 		background = 1;
-		args[numArgs-1] = NULL;	
+		args[numArgs-1] = NULL;	//remove the '&'
 	}
 	else {
+		//foreground process
 		start = time(0);
 	}
 
 
-	//for parentprocess: returns pid of child 
-	//for childprocess: return 0 
+	//in parentprocess: fork returns pid of child 
+	//in childprocess: fork return 0 
 	pid = fork();	
-	
 	if(pid == 0) {
 		if(execvp(args[0], args) == -1)	//execvp only returns in case of error
 			perror("fork error");		//print last error encountered on stderr
-		exit(EXUT_FAILURE);	
+		exit(EXIT_FAILURE);	
 	} else if(pid == -1) {
 		perror("fork error");	
 	} else {
-		while((pid = waitpid(-1, &status, WUNTRACED)) > 0) {
-			if(WIFEXITED(status) || WIFSIGNALED(status)) 
-				stop = time(0);
-				totalTime = difftime(start, stop);
-				printf("Foreground process %d has been terminated, running time:%fms\n", pid, totalTime);
+		printf("pid pid pid pid%i", pid);
+		if(!background){
+			waitpid(-1, &status, 0);
+			stop = time(0);
+			totalTime = difftime(stop, start);
+			printf("Running time:%.4fms\n", totalTime);
 		}
-	}
+		while((pid = waitpid(-1, &status, WNOHANG)) > 0) {
+			printf("Sug balle3");
+			if(WIFEXITED(status) || WIFSIGNALED(status)) {
+				printf("Process %d has been terminated\n", pid);
+			}
+		}
+	}	
 	return 1;
 }
